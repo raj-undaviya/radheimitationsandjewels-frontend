2c294c5c81068f714b08909cc4009f4790253d46
akshita <akshitasondagar81@gmail.com>
2026-03-19 15:28:58 +0530

    remove hello world

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -135,7 +135,6 @@ export default function Cart() {
                                 <div className="text-right text-orange-500 font-bold">
                                     ₹{(item.price * item.qty).toLocaleString()}
                                 </div>
-                                <h1>Hello World Test...</h1>
 
                             </div>
 
